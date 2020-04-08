c6395a55792228b3f3679c25d149e03e30a88b8f
Martini Siu <martini@Martinis-MacBook-Pro.local>
2020-04-08 20:24:43 +0100

    Fixed search functionaility

--- nurse/models.py
+++ nurse/models.py
@@ -7,10 +7,12 @@ class Nurse(models.Model):
     firstName = models.CharField(max_length=100)
     lastName = models.CharField(max_length=100) 
     fullName = str(firstName) + " " + str(lastName)
-    postCode = models.CharField(max_length=10)
+    postCode = models.CharField(max_length=10, default="")
     area = models.CharField(max_length=100)
     phoneNumber = models.CharField(max_length=20)
     email = models.EmailField(max_length=100, null=True)
+    lattiude = models.FloatField(max_length=10, null = True)
+    longitude = models.FloatField(max_length=10, null =True)
 
     def __str__(self):
         return self.firstName
