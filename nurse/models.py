263c99675fa90461765d32d8a194347632a73126
Martini Siu <martinisiu14@gmail.com>
2020-04-14 19:42:54 +0100

    New lick of paint

--- nurse/models.py
+++ nurse/models.py
@@ -3,24 +3,33 @@ import json
 import requests
 
 # Create your models here.
+
 class Nurse(models.Model):
     firstName = models.CharField(max_length=100)
-    lastName = models.CharField(max_length=100) 
+    lastName = models.CharField(max_length=100)
     fullName = str(firstName) + " " + str(lastName)
     postCode = models.CharField(max_length=10, default="")
     area = models.CharField(max_length=100)
     phoneNumber = models.CharField(max_length=20)
     email = models.EmailField(max_length=100, null=True)
-    lattiude = models.FloatField(max_length=10, null = True)
-    longitude = models.FloatField(max_length=10, null =True)
+    lattiude = models.FloatField(max_length=10, null=True)
+    longitude = models.FloatField(max_length=10, null=True)
 
     def __str__(self):
         return self.firstName
-    
+
+    """
     def getArea(self, postcode):
         areaFile = requests.get('http://api.postcodes.io/postcodes/SE17XB')
         data = areaFile.json()
         for d in data['results']:
             print(p['primary_care_trust'])
 
-    
+    def get(postalcode):
+    apiCall = requests.get('http://api.postcodes.io/postcodes/' + postalcode)
+    data = apiCall.json()
+    latitude = data['result']['latitude']
+    longitude = data['result']['longitude']
+
+    return (latitude, longitude)
+    """
